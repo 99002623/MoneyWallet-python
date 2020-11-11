28502a9ed60e6f546969f36891fe143d245084d1
Naga Akhil ES <nagaakhil.es@ltts.com>
2020-11-11 15:52:23 +0530

    menu init

---
+++ menu1.py
@@ -0,0 +1,13 @@
+from login import login
+from create_account import create_account
+from read_file import read_file
+from menu2 import clear_screen
+
+accounts_list = read_file('Accounts.txt')
+
+
+
+
+
+if __name__ == '__main__':
+    menu1()
